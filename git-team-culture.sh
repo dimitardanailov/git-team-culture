721e6e5c65483f57b21480efea4fc736b07270b0
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-03-25 22:10:04 +0300

    Add feature: commitlint.

--- git-team-culture.sh
+++ git-team-culture.sh
@@ -2,4 +2,6 @@
 
 unlink ./.git/hooks/prepare-commit-msg
 
-ln -s ./.git-templates/hooks/prepare-commit-msg ./.git/hooks/prepare-commit-msg
+ln -s ./.git-templates/hooks/prepare-commit-msg ./.git/hooks/
+
+chmod +x ./.git/hooks/prepare-commit-msg
